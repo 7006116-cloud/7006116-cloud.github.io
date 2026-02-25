d074384fab16d4f0745786e55df185952fff9043
7006116-cloud <7006116@spsd.sk.ca>
2026-02-25 14:28:10 -0600

    update interactive

--- interactive_assi/sketch.js
+++ interactive_assi/sketch.js
@@ -17,17 +17,9 @@ let bishopimg;
 let rookimg;
 
 function setup() {
-  createCanvas(400, 400);
-  square_size = width / 8;
-  kingimg = image("/assets/king.png")
-}
-
-function draw() {
-  background(220);
-  background(220);
-  
-  
-  
+  createCanvas(windowWidth, windowHeight);
+  square_size = height / 8;
+  // kingimg = image("/assets/king.png");
   for (let row = 0; row < 8; row++) {
     for (let col = 0; col < 8; col++) {
       if ((row + col) % 2 === 0) {
@@ -38,10 +30,9 @@ function draw() {
       rect(col * square_size, row * square_size, square_size, square_size);
     }
   }
-  
-  
-  
-  
+}
+
+function draw() {
 }
 
 
